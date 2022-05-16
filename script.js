8dc15110fb0fa79156a36d36797edbbd70dfa720
Alessia Belperio <abelperio.98@gmail.com>
2022-05-16 18:35:25 +0930

    first try for password length

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  var userPassword = window.prompt ("Enter a password length");
+
+  if (userPassword => 8) {
+    window.alert("Please enter a number between 8 and 128");
+    return;
+}
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
