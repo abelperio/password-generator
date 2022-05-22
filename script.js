ae9f09d947a6941c76c278956ee946dd40e8f93b
Alessia Belperio <abelperio.98@gmail.com>
2022-05-22 19:45:25 +0930

    added all prompts

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword () {
   var userPassword = window.prompt ("Enter a password length");
 
-  if (userPassword => 8) {
-    window.alert("Please enter a number between 8 and 128");
-    return;
+  // if (userPassword => 8 - 128) {
+  //   window.alert("Please enter a number between 8 and 128");
+  //   return;
+  // }
+
+  window.prompt ("Would you like to include lower case characters") 
+
+  window.prompt ("Would you like to include UPPER case characters") 
+
+  window.prompt ("Would you like to include n7m3r1c characters") 
+
+  window.prompt ("Would you like to include s?eci@l characters") 
 }
-}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
